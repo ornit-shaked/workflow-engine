5f6fa6272946eeb3a9e626dbeab2008e88373042
oshaked <oshaked@ecitele.com>
2020-01-19 21:32:23 +0200

    edit internal modules

--- workflow-embedded-engine-server/src/main/java/com/oshaked/camunda/controller/WorkflowController.java
+++ workflow-embedded-engine-server/src/main/java/com/oshaked/camunda/controller/WorkflowController.java
@@ -1,13 +1,8 @@
 package com.oshaked.camunda.controller;
 
+
 import com.ecitele.camunda.web.dto.InitiatePaymentDto;
 import com.oshaked.camunda.service.OrderProcessInstance;
-import org.camunda.bpm.engine.RepositoryService;
-import org.camunda.bpm.engine.repository.Deployment;
-import org.camunda.bpm.model.bpmn.Bpmn;
-import org.camunda.bpm.model.bpmn.BpmnModelInstance;
-import org.camunda.bpm.model.bpmn.impl.instance.ProcessImpl;
-import org.camunda.bpm.model.bpmn.instance.Process;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,7 +16,7 @@ public class WorkflowController {
 
     @PostMapping("/createInstance/{processId}")
     public String createInstance(@PathVariable("processId") String processId,
-                                             @RequestBody InitiatePaymentDto initiatePaymentDto) {
+                                 @RequestBody InitiatePaymentDto initiatePaymentDto) {
         System.out.println("Create Instance ...");
         String response = orderProcessInstance.create(processId, initiatePaymentDto);
 
